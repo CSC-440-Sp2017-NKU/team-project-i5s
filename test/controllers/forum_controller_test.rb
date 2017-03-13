efa0887360f2a362831af2c211d9a30fa2d2cb31
Robert <basszillion@gmail.com>
2017-03-13 22:18:52 +0000

    generated new forum controller

--- test/controllers/forum_controller_test.rb
+++ test/controllers/forum_controller_test.rb
@@ -1,32 +1,34 @@
 require 'test_helper'
 
 class ForumControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
   
-  #def setup
-    #@base_title = "Ruby on Rails Tutorial Sample App"
- # end
-  
-  test "should get root" do
-    get root_url
-    assert_response :success
-  end
-  
-  #test "should get home" do
-   # get forum_home_url
-    #assert_response :success
-    #assert_select "title", "#{@base_title}"
-  #end
-
-  test "should get view forum" do
-    get forum_view_forum_url
-    assert_response :success
-    #assert_select "title", "view forum | #{@base_title}"
-  end
-
-  test "should get index" do
-    get forum_index_url
-    assert_response :success
-    #assert_select "title", "index | #{@base_title}"
-  end
-  
+    def index
+    end
+    
+    def view_forum
+    end
+    
+    def list_forums
+    end
+    
+    test "should get index" do
+      get forum_index_url
+      assert_response :success
+      #assert_select "title", "view index| #{@base_title}"
+    end 
+    
+    test "should get view forum" do
+      get forum_view_forum_url
+      assert_response :success
+      #assert_select "title", "view view forum | #{@base_title}"
+    end 
+    
+    test "should get list forums" do
+      get forum_list_forums_url
+      assert_response :success
+      #assert_select "title", "view list forums | #{@base_title}"
+    end 
 end
