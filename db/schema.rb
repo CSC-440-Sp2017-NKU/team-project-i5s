33da170683ddd10d1553f4277808c0c67c3f941b
DC Gr <dcg.assoc@gmail.com>
2017-03-16 14:27:33 +0000

    gems updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170315033705) do
+ActiveRecord::Schema.define(version: 20170316140905) do
 
   create_table "answers", force: :cascade do |t|
     t.string   "text"
