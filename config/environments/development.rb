219a9db9f595a54615a6559bcba423c13de1cb31
DC Gr <dcg.assoc@gmail.com>
2017-03-16 11:40:01 +0000

    modified config to disable cahcing in dev mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,6 +47,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_controller.perform_caching = false # do not cache! added DCG: 03/13/2017
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
