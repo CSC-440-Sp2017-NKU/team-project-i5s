8be8b1731104936a017a37c12117a6783827d2af
Hunter Oka <okah1@nku.edu>
2017-03-17 18:00:13 +0000

    update deliverables code

--- deliverables/iteration1/Iteration1Final/test/controllers/forum_controller_test.rb
+++ deliverables/iteration1/Iteration1Final/test/controllers/forum_controller_test.rb
@@ -1,34 +1,23 @@
 require 'test_helper'
 
 class ForumControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
   
-    def index
-    end
-    
-    def view_forum
-    end
-    
-    def list_forums
-    end
-    
+   # def setup
+      #load "#{Rails.root}/db/seeds.rb"
+   # end
     test "should get index" do
       get forum_index_url
+      #get forum_index_url
       assert_response :success
-      #assert_select "title", "view index| #{@base_title}"
     end 
     
-    test "should get view forum" do
-      get forum_view_forum_url
-      assert_response :success
-      #assert_select "title", "view view forum | #{@base_title}"
-    end 
+  #  test "should get view forum" do
+     # get forum_view_forum_url #, params: {id: 1}
+     # assert_response :success
+  #  end 
     
-    test "should get list forums" do
-      get forum_list_forums_url
-      assert_response :success
-      #assert_select "title", "view list forums | #{@base_title}"
-    end 
+  #  test "should get forum count" do
+  #    i = Int(Forum.count)
+  #    assert i > 0
+  #  end
 end
