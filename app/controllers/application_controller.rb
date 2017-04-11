9c913f71582fb24bc4997bceb3d3854927e3f736
DC Gr <dcg.assoc@gmail.com>
2017-04-11 21:10:43 +0000

    fixed static posting questions/answers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,7 +29,7 @@ layout "main"
       return @current_user_session if defined?(@current_user_session)
       @current_user_session = UserSession.find
     end
-
+    
     def current_user
       return @current_user if defined?(@current_user)
       @current_user = current_user_session && current_user_session.user
