fed8751f308f0e2a6f5eeccbe7b8af28d431662d
Doug Bruening <brueningd1@nku.edu>
2017-04-28 02:19:53 +0000

    section methods

--- app/models/section.rb
+++ app/models/section.rb
@@ -5,6 +5,7 @@ class Section < ApplicationRecord
   has_many :student_sections
   has_many :users, :through => :student_sections
   has_many :courses, :through => :student_sections
+  has_one :users
   #sections are taught by instructors
   
   #TODO: return students enrolled in this section
@@ -12,7 +13,10 @@ class Section < ApplicationRecord
    #return Section.Users.where(:users => )  #joins(:users).where(:users => {:id => self.id}).id
     #return Section.joins(:users).where(:users => {:id => self.id}).all
     #return User.where(:id => 1)
-    return User.joins(:student_sections).where(:student_sections.user_id => 14) #where(:sections => {:id => :student_section.section_id}, :users => {:id => :student_section.user_id}) #
+    #return User.joins(:student_sections).where(:student_sections.user_id => 14) #where(:sections => {:id => :student_section.section_id}, :users => {:id => :student_section.user_id}) #
+    #User.joins(:sections).where(:users => {:id => student_sections.@user_id} and :student_section => {:id => self.id}).all
+    User.joins(:sections).where(:sections => {:id => self.id}).all
+    #Course.joins(:users).where(:users => {:id => self.id}).all.uniq
   end
   
   #TODO: return instructor for this section
