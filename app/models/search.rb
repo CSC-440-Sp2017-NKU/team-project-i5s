224acefb711d79d4102ee7c8e9d8f7fc024a0f9b
DC Gr <dcg.assoc@gmail.com>
2017-05-03 23:59:43 +0000

    search is working

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,73 +1,70 @@
 class Search < ApplicationRecord
-    def self.search_questions
+  
+    def self.search_courses(query_criteria)
+      #make sure we passing in values to the model
+      #course :: name, instructor
+     # query_criteria = {:course_instructor_name => "Fac1"}
+      
+      if !query_criteria.empty? and (query_criteria.has_key?(:course_instructor_name) or query_criteria.has_key?(:course_name))
+        return course_results(query_criteria) #.to_h
+      else
+        return []
+      end
+    end
+    
+    def self.search_questions(query_criteria)
 #make sure we passing in values to the model
-      query_criteria = {:question_user_name => "Harry"}
+     # query_criteria = {:question_user_name => "Harry"}
       
       if !query_criteria.empty? and (query_criteria.has_key?(:question_user_name) or query_criteria.has_key?(:question_title) or query_criteria.has_key?(:question_subject) or query_criteria.has_key?(:question_keyword))
         return question_results(query_criteria) #.to_h
       else
-        #return [] #.to_h
+        return []
       end
     end
-    
-    
-    
+
 private
+  def self.course_results(query_criteria)
+    #query the DB for all questions/keywords/users
+    #and whittle down the results  based on teh passed-in parameters
+       #course :: name, instructor
+        #COURSE  -> course_name
+        #SECTION -> instructor_id
+      #  USER id -> id, user_name
+
+        sSQL = %{SELECT DISTINCT  
+                   COURSES.ID as COURSE_ID, COURSES.COURSE_NAME, SECTIONS.ID as SECTION_ID, SECTIONS.USER_ID as COURSE_INSTRUCTOR_ID, USERS.USER_NAME as COURSE_INSTRUCTOR_NAME
+                 FROM
+                   COURSES
+                   LEFT JOIN 
+                   (SECTIONS 
+                    INNER JOIN
+                    USERS ON SECTIONS.USER_ID = USERS.ID 
+                   ) ON COURSES.ID = SECTIONS.COURSE_ID
+                }
+                
+        #build our where clause dynamically
+        #course :: name, instructor
+        if query_criteria.has_key?(:course_instructor_name) and query_criteria.has_key?(:course_name) 
+          sWhere = "INSTR(USERS.USER_NAME, :course_instructor_name) > 0 AND INSTR(COURSES.COURSE_NAME, :course_name) > 0" if query_criteria.has_key?(:course_instructor_name)
+           
+        else
+          sWhere = "INSTR(USERS.USER_NAME, :course_instructor_name) > 0" if query_criteria.has_key?(:course_instructor_name)
+          sWhere = sWhere + " AND INSTR(COURSES.COURSE_NAME, :course_name) > 0" if query_criteria.has_key?(:course_name) 
+        end
+        
+        #return our results
+    #this workeD!!!!
+    #http://stackoverflow.com/questions/28367495/avoid-sql-injection-with-connection-execute
+      query = sanitize_sql([sSQL + sWhere, query_criteria])
+      return ActiveRecord::Base.connection.execute(query)
+    end
+
+
     def self.question_results(query_criteria)
     #query the DB for all questions/keywords/users
     #and whittle down the results based on teh passed-in parameters
-        # sSQL = %{SELECT   
-        #           Q.ID as QUESTION_ID, Q.TITLE as QUESTION_TITLE, Q.TEXT as QUESTION_TEXT, Q.USER_ID as QUESTION_USER_ID, U.USER_NAME as QUESTION_USER_NAME, K.KEYWORD AS QUESTION_KEYWORD 
-        #         FROM   
-        #           QUESTIONS Q   
-        #         INNER JOIN 
-        #           USERS U ON Q.USER_ID=U.ID    
-        #         LEFT JOIN  
-        #           (SELECT 
-        #             QW.QUESTION_ID, QWK.KEYWORD 
-        #           FROM 
-        #           QUESTION_KEYWORDS QW INNER JOIN KEYWORDS QWK ON QW.KEYWORD_ID = QWK.ID 
-        #           )   K ON Q.ID = K.QUESTION_ID
-        #         } 
-        # sSQL = %{SELECT   
-        #           Q.ID, Q.TITLE, Q.TEXT, Q.USER_ID, U.USER_NAME, K.KEYWORD 
-        #         FROM   
-        #           QUESTIONS Q   
-        #         INNER JOIN 
-        #           USERS U ON Q.USER_ID = U.ID    
-        #         LEFT JOIN  
-        #           (SELECT 
-        #             QW.QUESTION_ID, QWK.KEYWORD 
-        #           FROM 
-        #           QUESTION_KEYWORDS QW INNER JOIN KEYWORDS QWK ON QW.KEYWORD_ID = QWK.ID 
-        #           )   K ON Q.ID = K.QUESTION_ID
-        #         } 
-        
-        sSQL = %{SELECT   
-                  QUESTIONS.ID, QUESTIONS.Title 
-                FROM   
-                  QUESTIONS   
-                INNER JOIN 
-                  USERS ON QUESTIONS.USER_ID = U.ID    
-                LEFT JOIN  
-                  (SELECT 
-                    QW.QUESTION_ID, QWK.KEYWORD 
-                  FROM 
-                  QUESTION_KEYWORDS QW INNER JOIN KEYWORDS QWK ON QW.KEYWORD_ID = QWK.ID 
-                  )   K ON QUESTIONS.ID = K.QUESTION_ID
-                } 
-        
-        sSQL2 = %{INNER JOIN 
-                  USERS ON QUESTIONS.USER_ID = USERS.ID    
-                LEFT JOIN  
-                  (SELECT 
-                    QW.QUESTION_ID, QWK.KEYWORD 
-                  FROM 
-                  QUESTION_KEYWORDS QW INNER JOIN KEYWORDS QWK ON QW.KEYWORD_ID = QWK.ID 
-                  )   K ON QUESTIONS.ID = K.QUESTION_ID}
-        sSELECT = %{QUESTIONS.ID, QUESTIONS.TITLE, QUESTIONS.TEXT, QUESTIONS.USER_ID, USERS.USER_NAME, K.KEYWORD}    
-        
-        sSQL3 = %{SELECT DISTINCT  
+        sSQL = %{SELECT DISTINCT  
                   QUESTIONS.ID as QUESTION_ID, QUESTIONS.TITLE as QUESTION_TITLE, QUESTIONS.TEXT as QUESTION_TEXT , QUESTIONS.USER_ID as QUESTION_USER_ID, USERS.USER_NAME as QUESTION_USER_NAME, K.KEYWORD as QUESTION_KEYWORD
                 FROM   
                   QUESTIONS   
@@ -89,25 +86,9 @@ private
         sWhere = sWhere + " AND INSTR(K.KEYWORD, :question_keyword) > 0" if query_criteria.has_key?(:question_keyword)    #, "%#{QUESITON_KEYWORD}%"    
        
         #return our results
-        #http://guides.rubyonrails.org/v3.2/active_record_querying.html 11.1 Using a String SQL Fragment
-       # x =  Search.find_by_sql([sSQL + sWhere, query_criteria])
-        #x = Question.joins(sSQL2).select(sSELECT).where(sWhere, query_criteria)
-        #x = Question.where(user_id:7, active: true) + User.where("instr(user_name, ?) > 0", "Harry") #+ Starter.search(search_term)
-       # return  ActiveRecord::Base.connection.find_by_sql [sSQL3 + sWhere, query_criteria]
-        #return  ActiveRecord::Base.connection.find_by_sql(sSQL3).where(sWhere, query_criteria) #]
-        #Search.execute(sSQL3 + sWhere, query_criteria)
-     # return ActiveRecord::Base.connection.exec_query(sSQL3) #.where(sWhere, query_criteria) #works
-     # return ActiveRecord::Base.connection.exec_query(sSQL3 + sWhere, query_criteria)
-      
-      #http://stackoverflow.com/questions/4483049/how-to-execute-a-raw-update-sql-with-dynamic-binding-in-rails/4484549#4484549
-      #st = ActiveRecord::Base.connection.raw_connection.prepare(sSQL3 + sWhere)
-      #st.execute(query_criteria)
-     # st.close
-    #return st
-    
     #this workeD!!!!
     #http://stackoverflow.com/questions/28367495/avoid-sql-injection-with-connection-execute
-      query = sanitize_sql([sSQL3 + sWhere, query_criteria])
+      query = sanitize_sql([sSQL + sWhere, query_criteria])
       return ActiveRecord::Base.connection.execute(query)
     end
 end
