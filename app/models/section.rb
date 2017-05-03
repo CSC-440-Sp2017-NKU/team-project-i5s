f02476a63c508db831cc82c39f598a0428b321ca
Hunter Oka <okah1@nku.edu>
2017-05-03 00:53:03 +0000

    fixing bits and pieces

--- app/models/section.rb
+++ app/models/section.rb
@@ -24,4 +24,13 @@ class Section < ApplicationRecord
     return Section.where(semester_id: self.id)
   end
   
+  def full_name
+    semester = Semester.find(self.semester_id)
+    return self.section_name + " " + semester.session.to_s + " " + semester.academic_year.to_s
+  end
+  
+  def course
+    return Course.find(self.course_id)
+  end
+  
 end
